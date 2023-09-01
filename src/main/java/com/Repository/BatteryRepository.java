a222bf6bdc6da96ecaf086162645319fc416230d
Noorish100 <mdnoorishnauman@gmail.com>
2023-09-01 16:34:17 +0530

    added third api logic

--- src/main/java/com/Repository/BatteryRepository.java
+++ src/main/java/com/Repository/BatteryRepository.java
@@ -2,6 +2,7 @@ package com.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,12 +14,8 @@ import com.entity.Battery;
 
 public interface BatteryRepository extends JpaRepository<Battery, Integer> {
 	
-//	@Query(value="select m.,m.primary_title,m.runtime_minutes,m.genres from Battery m order by m.runtime_minutes desc limit 10",nativeQuery = true) 
-//    List<Object> findByRuntimeMinutes();
-	        //  Substri   //          "bd.vol"
-	@Query(value="SELECT :s FROM Battery bd WHERE bd.Battery_ID = :id AND bd.Time >= :startTime AND bd.Time <= :endTime", nativeQuery=true)
-	Object findDataBetweenTimeRange(@Param("s") String s, @Param("id") Integer id, @Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
 
-
+	@Query(value = "SELECT  * FROM Battery bd WHERE bd.Battery_ID = :id AND bd.Time >= :startTime AND bd.Time <= :endTime", nativeQuery = true)
+	Optional<Battery> findDataBetweenTimeRange(@Param("id") Integer id, @Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime);
 	
 }
--- src/main/java/com/Service/BatteryServiceImpl.java
+++ src/main/java/com/Service/BatteryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,83 +14,106 @@ import com.exception.BatteryException;
 
 @Service
 public class BatteryServiceImpl implements BatteryService {
-	
-	String[] con= {"current","voltage","temperature","time"};
-	
+
+	String[] con = { "current", "voltage", "temperature", "time" };
+
 	@Autowired
 	private BatteryRepository batteryRepository;
 
-	
 	@Override
 	public String saveBattery(Battery battery) {
 		Optional<Battery> existedBattery = batteryRepository.findById(battery.getBatteryId());
-		if(existedBattery.isPresent()) {
+		if (existedBattery.isPresent()) {
 			return "already present";
 		}
-	     batteryRepository.save(battery);
+		batteryRepository.save(battery);
 		return "Battery data has been saved";
 	}
 
 	@Override
-	public Battery getInfoOfBattery(Integer id)throws BatteryException {
+	public Battery getInfoOfBattery(Integer id) throws BatteryException {
 		Optional<Battery> existedBattery = batteryRepository.findById(id);
-		
-		Battery b=existedBattery.get();
-		
-		if(existedBattery.isEmpty()) {
+
+		Battery b = existedBattery.get();
+
+		if (existedBattery.isEmpty()) {
 			throw new BatteryException("not Found");
 		}
 		return b;
 	}
 
 	@Override
-	public String getSpecificInfoOfBattery(Integer id, List<String> namesOfDetail)throws BatteryException {
-     Optional<Battery> existedBattery = batteryRepository.findById(id);
-		String ans="";
-		Battery b=existedBattery.get();
-		
-		if(existedBattery.isEmpty()) {
+	public String getSpecificInfoOfBattery(Integer id, List<String> namesOfDetail) throws BatteryException {
+		Optional<Battery> existedBattery = batteryRepository.findById(id);
+		String ans = "";
+		Battery b = existedBattery.get();
+
+		if (existedBattery.isEmpty()) {
 			throw new BatteryException("not Found");
 		}
-		for(int i=0;i<con.length;i++) {
-			if(namesOfDetail.contains(con[i])) {
-			if(con[i].equalsIgnoreCase("current")) {
-				ans=ans+" "+"Current"+"= "+b.getCurrent();
+		for (int i = 0; i < con.length; i++) {
+			if (namesOfDetail.contains(con[i])) {
+				if (con[i].equalsIgnoreCase("current")) {
+					ans = ans + " " + "Current" + "= " + b.getCurrent();
+				}
+				if (con[i].equalsIgnoreCase("voltage")) {
+					ans = ans + " " + "Voltage" + "= " + b.getVoltage();
+				}
+				if (con[i].equalsIgnoreCase("temperature")) {
+					ans = ans + " " + "Temperature" + "= " + b.getTemprature();
+				}
+				if (con[i].equalsIgnoreCase("time")) {
+					ans = ans + " " + "time" + "= " + b.getTime();
+				}
+
 			}
-		     if(con[i].equalsIgnoreCase("voltage")) {
-				ans=ans+" "+"Voltage"+"= "+b.getVoltage();
-			}
-		     if(con[i].equalsIgnoreCase("temperature")) {
-		    	 ans=ans+" "+"Temperature"+"= "+b.getTemprature();
-		     }
-		     if(con[i].equalsIgnoreCase("time")) {
-		    	 ans=ans+" "+"time"+"= "+b.getTime();
-		     }
-			  	
-		}}
-		return ans;	
+		}
+		return ans;
 	}
 
 	@Override
-	public Object getSpecificInfoOfBatteryInTimeRange(Integer id, String[] namesOfDetail, LocalDateTime startTime, LocalDateTime endTime) {
-		String s="";
-		for(int i=0;i<namesOfDetail.length;i++) {
-			if(i<namesOfDetail.length-1) {
-			s=s+"bd."+namesOfDetail[i]+",";
-			}
-			else {
-				s=s+"bd."+namesOfDetail[i];
-			}
-		}
-     
-     
-    
-		return batteryRepository.findDataBetweenTimeRange(s,id, startTime, endTime);
+	public Object getSpecificInfoOfBatteryInTimeRange(Integer id, String[] namesOfDetail, LocalDateTime startTime,
+			LocalDateTime endTime) {
+//		String s = "";
+//		for (int i = 0; i < namesOfDetail.length; i++) {
+//			if (i < namesOfDetail.length - 1) {
+//				s = s + "bd." + namesOfDetail[i] + ",";
+//			} else {
+//				s = s + "bd." + namesOfDetail[i];
+//			}
+//		}
 		
-		
-	
+		 Optional<Battery> batteryData = batteryRepository.findDataBetweenTimeRange(id, startTime, endTime);
+	        List<Object> result = new ArrayList<>();
+	        
+	      if(batteryData.isEmpty()){
+	    	  return "Sorry Query result empty set ";
+	      }
+	      Battery battery =batteryData.get();
+
+	        
+	        	for(int i=0;i<namesOfDetail.length;i++) {
+	            switch (namesOfDetail[i]) {
+	                case "current":
+	                    result.add("current="+battery.getCurrent());
+	                    break;
+	                case "voltage":
+	                    result.add("voltage="+battery.getVoltage());
+	                    break;
+	                case "time":
+	                    result.add("time="+battery.getTime());
+	                    break;
+	                case "temprature":
+	                    result.add("temprature="+battery.getTemprature());
+	                    break;
+	                // Add cases for other columns as needed
+	             
+	            }
+	        	}
+	        
+        
+	        return result;
+//	  return batteryRepository.findDataBetweenTimeRange(id, startTime, endTime);
 	}
-	
-	
 
 }
